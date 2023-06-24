de07cc30afd8f58ee8e91dcadd684765d732bb04
Сергей <bondarevser.bondarev@yandex.ru>
2023-06-25 00:19:51 +0300

    End

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
 
+     Random random = new Random();
+    Hogwarts potter = new Hogwarts("Potter","Гриффендор",
+            random.nextInt(101),
+            random.nextInt(101));
+
+    Slytherin drako = new Slytherin("Драко","Слизерин",
+            random.nextInt(101),
+            random.nextInt(101),
+            random.nextInt(101),
+            random.nextInt(101),
+            random.nextInt(101),
+            random.nextInt(101),
+            random.nextInt(101));
+        System.out.println(drako);
+        drako.compered(potter);
+
+}
 }
